96b9a43818f198f92ea0bc2667064671f933c06b
sammyspills <py14sts@leeds.ac.uk>
2016-08-21 01:50:19 +0100

    Further update of map screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -59,62 +59,94 @@ angular.module('app.controllers', [])
     console.log(divHeight + ',  ' + divWidth)
     
     var data = [{
-    "sale": "202",
-    "year": "2000"
+        "Client": "ABC",
+        "sale": "202",
+        "year": "2000"
     }, {
-    "sale": "215",
-    "year": "2001"
+        "Client": "ABC",
+        "sale": "215",
+        "year": "2002"
     }, {
-    "sale": "179",
-    "year": "2002"
+        "Client": "ABC",
+        "sale": "179",
+        "year": "2004"
     }, {
-    "sale": "199",
-    "year": "2003"
+        "Client": "ABC",
+        "sale": "199",
+        "year": "2006"
     }, {
-    "sale": "134",
-    "year": "2003"
+        "Client": "ABC",
+        "sale": "134",
+        "year": "2008"
     }, {
-    "sale": "176",
-    "year": "2010"
+        "Client": "ABC",
+        "sale": "176",
+        "year": "2010"
+    }, {
+        "Client": "XYZ",
+        "sale": "100",
+        "year": "2000"
+    }, {
+        "Client": "XYZ",
+        "sale": "215",
+        "year": "2002"
+    }, {
+        "Client": "XYZ",
+        "sale": "179",
+        "year": "2004"
+    }, {
+        "Client": "XYZ",
+        "sale": "199",
+        "year": "2006"
+    }, {
+        "Client": "XYZ",
+        "sale": "134",
+        "year": "2008"
+    }, {
+        "Client": "XYZ",
+        "sale": "176",
+        "year": "2013"
     }];
-    
-    var data2 = [{
-    "sale": "152",
-    "year": "2000"
-    }, {
-    "sale": "189",
-    "year": "2002"
-    }, {
-    "sale": "179",
-    "year": "2004"
-    }, {
-    "sale": "199",
-    "year": "2006"
-    }, {
-    "sale": "134",
-    "year": "2008"
-    }, {
-    "sale": "176",
-    "year": "2010"
-    }];
+
+    var dataGroup = d3.nest()
+    .key(function(d) {
+        return d.Client;
+    })
+    .entries(data);
 
     var vis = d3.select("#visualisation"),
         WIDTH = divWidth,
         HEIGHT = divHeight,
         MARGINS = {
-            top: 20,
+            top: 15,
             right: 20,
-            bottom: 20,
+            bottom: 70,
             left: 50
         },
-        xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]),
-        yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]),
+        xScale = d3.scaleLinear()
+            .range([MARGINS.left, WIDTH - MARGINS.right])
+            .domain([d3.min(data, function(d) {
+                return d.year;
+            }), d3.max(data, function(d) {
+                return d.year;
+            })]),
+
+        yScale = d3.scaleLinear()
+        .range([HEIGHT - MARGINS.bottom, MARGINS.top])
+        .domain([d3.min(data, function(d) {
+            return d.sale;
+        }), d3.max(data, function(d) {
+            return d.sale;
+        })]),
+
         xAxis = d3.axisBottom().scale(xScale),
         yAxis = d3.axisLeft().scale(yScale);
     
+    var lSpace = WIDTH/dataGroup.length;
+
     vis.append("svg:g")
     .attr("class","axis")
-    .attr("transform", "translate(0," +(HEIGHT - (2*MARGINS.bottom))+ ")")
+    .attr("transform", "translate(0," +(HEIGHT - (MARGINS.bottom))+ ")")
     .call(xAxis);
     
     vis.append("svg:g")
@@ -131,17 +163,35 @@ angular.module('app.controllers', [])
         })
         .curve(d3.curveBasis);
     
-    vis.append('svg:path')
-        .attr('d', lineGen(data))
-        .attr('stroke', 'green')
-        .attr('stroke-width', 2)
-        .attr('fill', 'none');
-    
-    vis.append('svg:path')
-      .attr('d', lineGen(data2))
-      .attr('stroke', 'blue')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none');
+    dataGroup.forEach(function(d, i) {
+        vis.append('svg:path')
+            .attr('d', lineGen(d.values))
+            .attr('stroke',  function(d, j) {
+                return "hsl(" + Math.random() * 360 + ",100%,50%)";
+            })
+            .attr('stroke-width', 2)
+            .attr('id', 'line_' + d.key)
+            .on ('click', function(){
+                alert(d.key);
+            })
+            .attr('fill', 'none');
+
+        vis.append("text")
+            .attr("x", (lSpace / 2) + i * lSpace)
+            .attr("y", (HEIGHT - (MARGINS.bottom/2)))
+            .style("fill", "black")
+            .attr("class", "legend")
+            .text(d.key)
+            .on('click', function(){
+                var active = d.active ? false : true;
+                var opacity = active ? 0 : 1;
+
+                d3.select("#line_" + d.key).style('opacity', opacity);
+
+                d.active = active;
+            });
+    });
+
 })
 
 .controller('mapsCtrl2', function($scope, $state, $cordovaGeolocation, $rootScope, $window) {	
@@ -284,4 +334,3 @@ angular.module('app.controllers', [])
 .controller('settingsCtrl', function($scope){
 	
 })
-    
