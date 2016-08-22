6bce86ecd2ef2b398e7242e83b59e306c6309e48
sammyspills <py14sts@leeds.ac.uk>
2016-08-22 20:42:57 +0100

    Updates 2 - 22/08/16
    Improved header. Got plotting to work with default CSV file.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,42 +13,53 @@ angular.module('app.controllers', [])
 })
    
 .controller('mapsCtrl', function($scope, $state, $cordovaGeolocation) {
+    
 
-		var options = {timeout: 10000, enableHighAccuracy: true};
- 
-		$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+    var options = {timeout: 10000, enableHighAccuracy: true};
+    //var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-			//var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            var latLng = new google.maps.LatLng(80, 0);
-            
-			var mapOptions = {
-				center: latLng,
-				zoom: 1,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
- 
-		$scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-		google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+    var latLng = new google.maps.LatLng(80, 0);
+    var mapOptions = {
+        center: latLng,
+        zoom: 1,
+        mapTypeId: 'terrain'
+    };
 
-			var marker = new google.maps.Marker({
-				map: $scope.map,
-				animation: google.maps.Animation.DROP,
-				position: latLng
-			})
-
-			var infoWindow = new google.maps.InfoWindow({
-      		content: "Current Location"
-  			});
- 
-  			google.maps.event.addListener(marker, 'click', function () {
-      		infoWindow.open($scope.map, marker);
-  			});
-
-		})
- 
-	}, function(error){
-		console.log("Could not get location");
-	});
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+//
+//        var marker = new google.maps.Marker({
+//            map: $scope.map,
+//            animation: google.maps.Animation.DROP,
+//            position: latLng
+//        })
+//
+//        var infoWindow = new google.maps.InfoWindow({
+//        content: "Current Location"
+//        });
+//
+//        google.maps.event.addListener(marker, 'click', function () {
+//        infoWindow.open($scope.map, marker);
+//        });
+//
+//    })
+    var latLngArray = []
+    var mapFunc = function(data){
+        data.forEach(function(d){
+            latLngArray.push({
+                lat: +d.lat,
+                lng: +d.lon
+            });
+        });
+    };
+    var shipTrack = new google.maps.Polyline({
+        path: latLngArray,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    shipTrack.setMap(map);
     
     var chartDiv = document.getElementById('chart-div');
     var divHeight = chartDiv.offsetHeight;
@@ -56,180 +67,119 @@ angular.module('app.controllers', [])
     
     var padding = "30";
     
-    console.log(divHeight + ',  ' + divWidth)
-    
-    var data = [{
-        "Client": "ABC",
-        "sale": "202",
-        "year": "2000"
-    }, {
-        "Client": "ABC",
-        "sale": "215",
-        "year": "2002"
-    }, {
-        "Client": "ABC",
-        "sale": "179",
-        "year": "2004"
-    }, {
-        "Client": "ABC",
-        "sale": "199",
-        "year": "2006"
-    }, {
-        "Client": "ABC",
-        "sale": "134",
-        "year": "2008"
-    }, {
-        "Client": "ABC",
-        "sale": "176",
-        "year": "2010"
-    }, {
-        "Client": "XYZ",
-        "sale": "100",
-        "year": "2000"
-    }, {
-        "Client": "XYZ",
-        "sale": "215",
-        "year": "2002"
-    }, {
-        "Client": "XYZ",
-        "sale": "179",
-        "year": "2004"
-    }, {
-        "Client": "XYZ",
-        "sale": "199",
-        "year": "2006"
-    }, {
-        "Client": "XYZ",
-        "sale": "134",
-        "year": "2008"
-    }, {
-        "Client": "XYZ",
-        "sale": "176",
-        "year": "2013"
-    }];
-
-    var dataGroup = d3.nest()
-    .key(function(d) {
-        return d.Client;
-    })
-    .entries(data);
-
-    var vis = d3.select("#visualisation"),
-        WIDTH = divWidth,
-        HEIGHT = divHeight,
-        MARGINS = {
-            top: 15,
-            right: 20,
-            bottom: 70,
-            left: 50
-        },
-        xScale = d3.scaleLinear()
-            .range([MARGINS.left, WIDTH - MARGINS.right])
-//            .domain([d3.min(data, function(d) {
-//                return d.year;
-//            }), d3.max(data, function(d) {
-//                return d.year;
-//            })]),
-
-        yScale = d3.scaleLinear()
-        .range([HEIGHT - MARGINS.bottom, MARGINS.top])
-//        .domain([d3.min(data, function(d) {
-//            return d.sale;
-//        }), d3.max(data, function(d) {
-//            return d.sale;
-//        })]),
-
-        xAxis = d3.axisBottom().scale(xScale),
-        yAxis = d3.axisLeft().scale(yScale);
-    
-    var lSpace = WIDTH/dataGroup.length;
-
-    vis.append("svg:g")
-    .attr("class","axis")
-    .attr("transform", "translate(0," +(HEIGHT - (MARGINS.bottom))+ ")")
-    .call(xAxis);
-    
-    vis.append("svg:g")
-    .attr("class","axis")
-    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-    .call(yAxis);
-    
-    var lineGen = d3.line()
-        .x(function(d) {
-            return xScale(d.year);
-        })
-        .y(function(d) {
-            return yScale(d.sale);
-        })
-        .curve(d3.curveBasis);
-    
     //Initialise distance calculating function:
-    var getDistance = function(latlon){
-        var lat_point = latlon[0];
-        var lon_point = latlon[1];
-        var deg2rad = Math.pi/180;
+//    var getDistance = function(latlon){
+//        var lat_point = latlon[0];
+//        var lon_point = latlon[1];
+//        var deg2rad = Math.pi/180;
+//        
+//        for(i = 0; i = (latlon[0].length - 1); i++){
+//            console.log((i*100/latlon[0].length) + "%");
+//            
+//            //phi = 90 - lat
+//            var phi1 = (90.0 - lat_point[i]) * deg2rad;
+//            var phi2 = (90.0 - lat_point[i-1]) * deg2rad;
+//            
+//            //theta = lon
+//            var theta1 = lon_point[i] * deg2rad;
+//            var theta2 = lon_point[i-1] * deg2rad;
+//            
+//            var cos = (Math.sin(phi1) * Math.sin(phi2) * Math.cos(theta1 - theta2)) + (Math.cos(phi1) * Math.cos(phi2));
+//            var arc = Math.acos(cos)*6371; //Radius of Earth.
+//            latlon[2].push(arc);
+//        };
+//        
+//        for(i = 1; i = (latlon[0].length - 1); i++){
+//            var x = latlon[3][i-1] + latlon[2][i];
+//            latlon[3].push(x);
+//        };
+//        console.log(latlon[3]);
+//    };
+    
+    var plotFunc = function(data){
+        //Initialise 2D array for lat, lon, dist., cum. dist.
+        var route_array_scientific = [[],[]];
         
-        for(i = 0; i = (latlon[0].length - 1); i++){
-            console.log((i*100/latlon[0].length) + "%");
-            
-            //phi = 90 - lat
-            var phi1 = (90.0 - lat_point[i]) * deg2rad;
-            var phi2 = (90.0 - lat_point[i-1]) * deg2rad;
-            
-            //theta = lon
-            var theta1 = lon_point[i] * deg2rad;
-            var theta2 = lon_point[i-1] * deg2rad;
-            
-            var cos = (Math.sin(phi1) * Math.sin(phi2) * Math.cos(theta1 - theta2)) + (Math.cos(phi1) * Math.cos(phi2));
-            var arc = Math.acos(cos)*6371; //Radius of Earth.
-            latlon[2].push(arc);
-        };
+        //append appropriate data to array
+        data.forEach(function(d){
+            if(d.thick >= 0){
+                route_array_scientific[1].push(d.cumdist);
+                route_array_scientific[0].push(d.thick);
+            } else {
+                
+            }
+        });
         
-        for(i = 1; i = (latlon[0].length - 1); i++){
-            var x = latlon[3][i-1] + latlon[2][i];
-            latlon[3].push(x);
-        };
-        console.log(latlon[3]);
+        var xData = route_array_scientific[1];
+        var yData = route_array_scientific[0];
+        
+        var vis = d3.select("#visualisation"),
+            WIDTH = divWidth,
+            HEIGHT = divHeight,
+            MARGINS = {
+                top: 15,
+                right: 20,
+                bottom: 50,
+                left: 60
+            },
+            xScale = d3.scaleLinear()
+            .range([MARGINS.left, WIDTH - MARGINS.right])
+            .domain([0, Math.max(...xData)]),
+
+            yScale = d3.scaleLinear()
+            .range([HEIGHT - MARGINS.bottom, MARGINS.top])
+            .domain([0, Math.max(...yData)]),
+
+            xAxis = d3.axisBottom().scale(xScale).tickArguments([20, "s"]),
+            yAxis = d3.axisLeft().scale(yScale).tickArguments([5, "s"]);
+
+        vis.append("svg:g")
+            .attr("class","axis")
+            .attr("transform", "translate(0," +(HEIGHT - (MARGINS.bottom))+ ")")
+            .call(xAxis);
+
+            vis.append("svg:g")
+            .attr("class","axis")
+            .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+            .call(yAxis);
+
+        var lineGen = d3.line().x(function(d, i) {
+                return xScale(xData[i]);
+            })
+            .y(function(d, i) {
+                return yScale(yData[i]);
+            });
+
+        // Add the valueline path.
+        vis.append("svg:path")
+        .attr("class", "line")
+        .attr("stroke", "#00376d")
+        .attr('stroke-width', 1)
+        .attr("d", lineGen(xData))
+        .attr("fill", "none");
+        
+        vis.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "middle")
+        .attr("x", MARGINS.left + (WIDTH-MARGINS.left-MARGINS.right)/2)
+        .attr("y", HEIGHT-(MARGINS.bottom/3))
+        .text("Distance along route (m)");
+        
+        vis.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "middle")
+        .attr("y", MARGINS.left/2.5)
+        .attr("x", -MARGINS.top-(HEIGHT-MARGINS.top-MARGINS.bottom)/2)
+        .attr("transform", "rotate(-90)")
+        .text("life expectancy (years)");
+
     };
     
-//Initialise 2D array for lat, lon, dist., cum. dist.
-    var route_array_scientific = [[],[],[],[0]];
-    d3.csv("res/filtered_route.csv", function(data){
-        data.forEach(function(d){
-            route_array_scientific[0].push(parseFloat(d.lat));
-            route_array_scientific[1].push(parseFloat(d.lon));
-        });
-        getDistance(route_array_scientific);
+    d3.csv("res/scientific_route.csv", function(d){
+        plotFunc(d);
+        mapFunc(d);
     });
-    
-    dataGroup.forEach(function(d, i) {
-        vis.append('svg:path')
-            .attr('d', lineGen(d.values))
-            .attr('stroke',  function(d, j) {
-                return "hsl(" + Math.random() * 360 + ",100%,50%)";
-            })
-            .attr('stroke-width', 2)
-            .attr('id', 'line_' + d.key)
-            .on ('click', function(){
-                alert(d.key);
-            })
-            .attr('fill', 'none');
-
-        vis.append("text")
-            .attr("x", (lSpace / 2) + i * lSpace)
-            .attr("y", (HEIGHT - (MARGINS.bottom/2)))
-            .style("fill", "black")
-            .attr("class", "legend")
-            .text(d.key)
-            .on('click', function(){
-                var active = d.active ? false : true;
-                var opacity = active ? 0 : 1;
-
-                d3.select("#line_" + d.key).style('opacity', opacity);
-
-                d.active = active;
-            });
-    });
+        
 
 })
 
@@ -269,7 +219,6 @@ angular.module('app.controllers', [])
 
 	  	$scope.isRecording = false;
 
-	  	// TODO: Set interval to 1 hour. Set debug = false.
 	  	bgGeo.configure({
 	     	//Both
 	     	desiredAccuracy: 20, // Desired Accuracy of the location updates (lower means more accurate but more battery consumption)
